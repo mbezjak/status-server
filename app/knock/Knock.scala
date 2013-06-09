230c888c4d7d86bafcb60b9c7b7f428efd5d3383
Miro Bezjak <bezjak.miro@gmail.com>
2013-06-09 14:23:51 +0200

    The simplest way of determining the availability of a web server.

---
+++ app/knock/Knock.scala
@@ -0,0 +1,14 @@
+package knock
+
+import play.api.libs.ws.{WS, Response}
+import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
+
+class Knock {
+
+  def available(url: String): Future[Boolean] =
+    request(url).map(_ => true).recover { case _ => false }
+
+  def request(url: String): Future[Response] = WS.url(url).head()
+
+}
