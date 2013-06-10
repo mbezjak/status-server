95a46874a2d0aa1f3ac9aeb052fd021a04da851f
Miro Bezjak <bezjak.miro@gmail.com>
2013-06-10 22:03:14 +0200

    Recover only from java.net.ConnectException.

--- app/knock/HttpKnock.scala
+++ app/knock/HttpKnock.scala
@@ -3,11 +3,12 @@ package knock
 import play.api.libs.ws.{WS, Response}
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
+import java.net.ConnectException
 
 class HttpKnock {
 
   def available(url: String): Future[Boolean] =
-    request(url).map(_ => true).recover { case _ => false }
+    request(url).map(_ => true).recover { case _: ConnectException => false }
 
   def request(url: String): Future[Response] = WS.url(url).head()
 
