b4b8173f0cc391443a6be5457ac1c84aff371c3b
Miro Bezjak <bezjak.miro@gmail.com>
2013-07-10 21:40:17 +0200

    Allow to check availability of a sequence of URLs.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,18 +3,20 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
+import scala.concurrent.Future
 
 import knock.HttpKnock
 
 object Application extends Controller {
 
   def index = Action {
-    val url    = "http://google.com"
-    val knock  = new HttpKnock
-    val future = knock available url
+    val urls    = Seq("http://google.com", "http://wikipedia.com")
+    val knock   = new HttpKnock
+    val futures = urls map { url => knock available url }
+    val future  = Future.sequence(futures)
 
     Async {
-      future map (views.html.index(url)) map (Ok(_))
+      future map (views.html.index(urls)) map (Ok(_))
     }
   }
 
