0a3e52483428e29ce5581473e5b22089893ad123
Miro Bezjak <bezjak.miro@gmail.com>
2013-07-22 12:21:29 +0200

    Treat TimeoutException as not available.

--- app/knock/HttpKnock.scala
+++ app/knock/HttpKnock.scala
@@ -2,6 +2,7 @@ package knock
 
 import com.ning.http.client.Realm.AuthScheme
 import java.net.ConnectException
+import java.util.concurrent.TimeoutException
 
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.{Response, WS}
@@ -10,7 +11,10 @@ import scala.concurrent.Future
 class HttpKnock {
 
   def available(url: String): Future[Boolean] =
-    request(url).map(_ => true).recover { case _: ConnectException => false }
+    request(url).map(_ => true).recover {
+      case _: ConnectException => false
+      case _: TimeoutException => false
+    }
 
   def request(url: String): Future[Response] =
     WS.url(url).
