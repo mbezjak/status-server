4a2d55fd061fcb576c46f9da78cc98cd588207f0
Miro Bezjak <bezjak.miro@gmail.com>
2013-07-22 11:58:41 +0200

    Do not attempt to use authentication while knocking.

--- app/knock/HttpKnock.scala
+++ app/knock/HttpKnock.scala
@@ -1,6 +1,8 @@
 package knock
 
+import com.ning.http.client.Realm.AuthScheme
 import java.net.ConnectException
+
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.{Response, WS}
 import scala.concurrent.Future
@@ -10,6 +12,7 @@ class HttpKnock {
   def available(url: String): Future[Boolean] =
     request(url).map(_ => true).recover { case _: ConnectException => false }
 
-  def request(url: String): Future[Response] = WS.url(url).head()
+  def request(url: String): Future[Response] =
+    WS.url(url).withAuth("none", "none", AuthScheme.NONE).head()
 
 }
