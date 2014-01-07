6b1d8f6090c7fb4b72163d3aded1ac2014a15d48
Miro Bezjak <bezjak.miro@gmail.com>
2014-01-07 16:38:44 +0100

    RELEASE 0.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "status-server"
-  val appVersion      = "0.1"
+  val appVersion      = "0.1.1"
 
   val appDependencies = Seq()
 
